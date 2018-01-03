e5fb94de1e5f9fa93d0e70239655553d916ca3ed
Makoto Uwaya <makoto.uwaya@gmail.com>
2018-01-03 10:26:04 +0900

    TemplateMethodパターンのサンプルコードを追加

---
+++ TemplateMethod/Tea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TemplateMethod
+{
+    public class Tea : CaffeineBeverage
+    {
+        protected override void AddCondiments()
+        {
+            Console.WriteLine("レモンを追加します");
+        }
+
+        protected override void Brew()
+        {
+            Console.WriteLine("紅茶を浸します");
+        }
+    }
+}
